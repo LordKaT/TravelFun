d506df2c583861dffc7fa94643ab52ac01acd3a1
Felicia Mirabel <lordkat@gmail.com>
2025-04-17 07:02:16 -0400

    I-84 Exit 15 A/B, Exit 19A/B

---
+++ pyscript/deprecations-manager-CrzZdV7R.js
@@ -0,0 +1,2 @@
+import{notify as e}from"./error-B9k1ZBzQ.js";import{e as o}from"./core-BzGjWUrB.js";function r(){const e=document.querySelectorAll("script");for(const o of e)s(o.src)}function s(o){/\/pyscript\.net\/latest/.test(o)&&e("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.")}o.main.onReady.add(r),o.main.onWorker.add(r);
+//# sourceMappingURL=deprecations-manager-CrzZdV7R.js.map
